914ea10d5b999f83d8e0504866e4a412ab6271b3
ernestorcornejo <147128656+ernestorcornejo@users.noreply.github.com>
2024-02-11 19:30:33 -0800

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,11 @@
 i = 0#python counts from 1
 while i < 100:#python stops exactly at 100 when using <
     i = i + 1#increment the while loop by 1
-    if i % 15 == 0:
+    if i % 15 == 0:# Check if divisible by 15 using modulo
         print("FizzBuzz")
-    elif i % 3 == 0:
+    elif i % 3 == 0:# Check divisibility by 3 using elif  
         print("Fizz")
-    elif i % 5 == 0:
+    elif i % 5 == 0:# Similar check if divisible by 5
         print("Buzz")
     else:
-        print (i)
+        print (i)# Print the number itself if none of the above
